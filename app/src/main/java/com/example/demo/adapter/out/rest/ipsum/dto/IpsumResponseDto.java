689d57e1df2fd379af99cf42f35333b975d5691e
Fernando Hidemi Uchiyama <fernandoservico@gmail.com>
2020-03-05 17:18:30 -0300

    Create IpsumResponseDto.java

---
+++ app/src/main/java/com/example/demo/adapter/out/rest/ipsum/dto/IpsumResponseDto.java
@@ -0,0 +1,14 @@
+package com.example.demo.adapter.out.rest.ipsum.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class IpsumResponseDto {
+    String title = "";
+
+    String body = "";
+
+    String image = "";
+}
