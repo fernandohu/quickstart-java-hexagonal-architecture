45e056ffa4063dd1da594ea48a52d1f569adadc5
Fernando Hidemi Uchiyama <fernandoservico@gmail.com>
2020-03-05 17:14:38 -0300

    Create IpsumControllerRequestDto.java

---
+++ app/src/main/java/com/example/demo/adapter/in/rest/ipsum/dto/IpsumControllerRequestDto.java
@@ -0,0 +1,10 @@
+package com.example.demo.adapter.in.rest.ipsum.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class IpsumControllerRequestDto {
+    int paragraphs = 1;
+}
